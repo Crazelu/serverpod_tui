730f11a6fa650cad19010e7d6848515590ccb0ce
Lucky Ebere <58946834+Crazelu@users.noreply.github.com>
2026-06-16 14:43:40 +0100

    fix: Improved multi screen form UI (#21)
    * chore: remove FormState.multiScreen constructor
    
    * fix: remove excessive whitespace from multi screen summary UI
    
    * fix: add right padding to multi screen navigation buttons for a better look on most terminals
    
    * chore: bump version and update changelog
    
    * chore: bump version to 0.4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.4.0
+
+- **FIX**: Removed excessive whitespace from multi screen form summary.
+- **FIX**: Added right padding to multi screen form navigation buttons.
+- **FIX**: Removed `FormState.multiScreen` constructor which did not enforce the requirement of `MultiScreenFormState` by `Form.multiScreen`.
+
 ## 0.3.0
 
 - **FEAT**: Added multi screen form support.
--- lib/src/components/form/multi_screen_form.dart
+++ lib/src/components/form/multi_screen_form.dart
@@ -135,6 +135,7 @@ class _MultiScreenNavigationButtons extends StatelessComponent {
             },
             focused: state.focusOnButton && state.focusedButtonIndex == 1,
           ),
+          const SizedBox(width: 1),
         ],
       ),
     );
@@ -168,13 +169,8 @@ class _SummaryScreen extends StatelessComponent {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(height: 1),
                   for (final config in configs)
-                    _SummaryItem(
-                      state: state,
-                      config: config,
-                    ),
-                  const SizedBox(height: 1),
+                    _SummaryItem(state: state, config: config),
                   if (description case String description) ...[
                     const SizedBox(height: 1),
                     Text(
