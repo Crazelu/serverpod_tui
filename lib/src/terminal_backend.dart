28f7847a831edcf504ba3f35d9d124907dd148dd
Crazelu <eberelucky64@gmail.com>
2026-07-10 19:35:03 +0100

    feat: add exit code to ServerpodTerminalBackend preExit

--- lib/src/terminal_backend.dart
+++ lib/src/terminal_backend.dart
@@ -5,11 +5,11 @@ import 'package:nocterm/nocterm.dart';
 class ServerpodTerminalBackend extends StdioBackend {
   ServerpodTerminalBackend({required this.preExit});
 
-  final Future<void> Function() preExit;
+  final Future<void> Function(int exitCode) preExit;
 
   @override
   void requestExit([int exitCode = 0]) {
-    preExit()
+    preExit(exitCode)
         .then((_) => super.requestExit(exitCode))
         .catchError((_) => super.requestExit(exitCode));
   }
